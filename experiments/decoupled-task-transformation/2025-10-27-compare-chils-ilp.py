29e8727f1060c7f7d52a2c0e30bf40c37cf8a2df
LazyStability <LazilyStable@proton.me>
2025-11-10 10:54:03 +0100

    chore: Working local & remote test suites

--- experiments/decoupled-task-transformation/2025-10-27-compare-chils-ilp.py
+++ experiments/decoupled-task-transformation/2025-10-27-compare-chils-ilp.py
@@ -55,44 +55,33 @@ ATTRIBUTES = [
 print("Is this the bwCluster? Answer: ",BWUniEnvironment.is_present())
 if BWUniEnvironment.is_present():
     # Satisficing
-    # SUITE = [
-    #     "agricola-sat18-strips", "airport", "assembly", "barman-sat11-strips",
-    #     "barman-sat14-strips", "blocks", "caldera-sat18-adl",
-    #     "caldera-split-sat18-adl", "cavediving-14-adl", "childsnack-sat14-strips",
-    #     "citycar-sat14-adl", "data-network-sat18-strips", "depot", "driverlog",
-    #     "elevators-sat08-strips", "elevators-sat11-strips", "flashfill-sat18-adl",
-    #     "floortile-sat11-strips", "floortile-sat14-strips", "freecell",
-    #     "ged-sat14-strips", "grid", "gripper", "hiking-sat14-strips",
-    #     "logistics00", "logistics98", "maintenance-sat14-adl", "miconic",
-    #     "miconic-fulladl", "miconic-simpleadl", "movie", "mprime", "mystery",
-    #     "nomystery-sat11-strips", "nurikabe-sat18-adl", "openstacks",
-    #     "openstacks-sat08-adl", "openstacks-sat08-strips",
-    #     "openstacks-sat11-strips", "openstacks-sat14-strips", "openstacks-strips",
-    #     "optical-telegraphs", "organic-synthesis-sat18-strips",
-    #     "organic-synthesis-split-sat18-strips", "parcprinter-08-strips",
-    #     "parcprinter-sat11-strips", "parking-sat11-strips", "parking-sat14-strips",
-    #     "pathways", "pegsol-08-strips", "pegsol-sat11-strips", "philosophers",
-    #     "pipesworld-notankage", "pipesworld-tankage", "psr-large", "psr-middle",
-    #     "psr-small", "rovers", "satellite", "scanalyzer-08-strips",
-    #     "scanalyzer-sat11-strips", "schedule", "settlers-sat18-adl",
-    #     "snake-sat18-strips", "sokoban-sat08-strips", "sokoban-sat11-strips",
-    #     "spider-sat18-strips", "storage", "termes-sat18-strips",
-    #     "tetris-sat14-strips", "thoughtful-sat14-strips", "tidybot-sat11-strips",
-    #     "tpp", "transport-sat08-strips", "transport-sat11-strips",
-    #     "transport-sat14-strips", "trucks", "trucks-strips",
-    #     "visitall-sat11-strips", "visitall-sat14-strips",
-    #     "woodworking-sat08-strips", "woodworking-sat11-strips", "zenotravel",
-    # ]
     SUITE = [
-        "depot:p01.pddl",
-        "driverlog:p01.pddl",
-        # "elevators-*",
-        # "logistics00",
-        # "miconic",
-        # "nomystery-*",
-        # "openstacks-{opt/sat}XY-strips",
-        # "*transport-*",
-        # "zenotravel"
+        "agricola-sat18-strips", "airport", "assembly", "barman-sat11-strips",
+        "barman-sat14-strips", "blocks", "caldera-sat18-adl",
+        "caldera-split-sat18-adl", "cavediving-14-adl", "childsnack-sat14-strips",
+        "citycar-sat14-adl", "data-network-sat18-strips", "depot", "driverlog",
+        "elevators-sat08-strips", "elevators-sat11-strips", "flashfill-sat18-adl",
+        "floortile-sat11-strips", "floortile-sat14-strips", "freecell",
+        "ged-sat14-strips", "grid", "gripper", "hiking-sat14-strips",
+        "logistics00", "logistics98", "maintenance-sat14-adl", "miconic",
+        "miconic-fulladl", "miconic-simpleadl", "movie", "mprime", "mystery",
+        "nomystery-sat11-strips", "nurikabe-sat18-adl", "openstacks",
+        "openstacks-sat08-adl", "openstacks-sat08-strips",
+        "openstacks-sat11-strips", "openstacks-sat14-strips", "openstacks-strips",
+        "optical-telegraphs", "organic-synthesis-sat18-strips",
+        "organic-synthesis-split-sat18-strips", "parcprinter-08-strips",
+        "parcprinter-sat11-strips", "parking-sat11-strips", "parking-sat14-strips",
+        "pathways", "pegsol-08-strips", "pegsol-sat11-strips", "philosophers",
+        "pipesworld-notankage", "pipesworld-tankage", "psr-large", "psr-middle",
+        "psr-small", "rovers", "satellite", "scanalyzer-08-strips",
+        "scanalyzer-sat11-strips", "schedule", "settlers-sat18-adl",
+        "snake-sat18-strips", "sokoban-sat08-strips", "sokoban-sat11-strips",
+        "spider-sat18-strips", "storage", "termes-sat18-strips",
+        "tetris-sat14-strips", "thoughtful-sat14-strips", "tidybot-sat11-strips",
+        "tpp", "transport-sat08-strips", "transport-sat11-strips",
+        "transport-sat14-strips", "trucks", "trucks-strips",
+        "visitall-sat11-strips", "visitall-sat14-strips",
+        "woodworking-sat08-strips", "woodworking-sat11-strips", "zenotravel",
     ]
     ENVIRONMENT = BWUniEnvironment(
         email="qf226@stud.uni-heidelberg.de",
@@ -104,27 +93,30 @@ else:
     SUITE = [
         "depot:p01.pddl",
         "driverlog:p01.pddl",
-        # "elevators-*",
-        # "logistics00",
-        # "miconic",
-        # "nomystery-*",
-        # "openstacks-{opt/sat}XY-strips",
-        # "*transport-*",
-        # "zenotravel"
+        "elevators-opt08-strips:p01.pddl",
+        "elevators-opt11-strips:p01.pddl",
+        "elevators-sat08-strips:p01.pddl",
+        "logistics00:probLOGISTICS-10-0.pddl",
+        "miconic:s1-0.pddl",
+        "nomystery-opt11-strips:p01.pddl",
+        "nomystery-sat11-strips:p01.pddl",
+        "openstacks-opt08-strips:p01.pddl",
+        "openstacks-opt11-strips:p01.pddl",
+        # "openstacks-opt14-strips:p01.pddl",
+        "openstacks-sat08-strips:p01.pddl",
+        "openstacks-sat11-strips:p01.pddl",
+        # "openstacks-sat14-strips:p01.pddl",
+        "transport-opt08-strips:p01.pddl",
+        "transport-opt11-strips:p01.pddl",
+        "transport-opt14-strips:p01.pddl",
+        "transport-sat08-strips:p01.pddl",
+        "transport-sat14-strips:p01.pddl",
+        "zenotravel:p01.pddl"
+
+        # Take longer
+        "elevators-sat11-strips:p01.pddl",
+        "transport-sat11-strips:p01.pddl",
     ]
-
-# exp.add_suite(benchmarks_dir, [
-#                                "depot",
-#                                "driverlog",
-#                                "elevators-*",
-#                                "logistics00",
-#                                "miconic",
-#                                "nomystery-*",
-#                                "openstacks-{opt/sat}XY-strips",
-#                                "*transport-*",
-#                                "zenotravel"
-#                                ])
-
 
 exp = FastDownwardExperiment(environment=ENVIRONMENT)
 
