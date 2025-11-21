36b0152e52c11fc79dc37c783e8ad759c51bffdc
LazyStability <LazilyStable@proton.me>
2025-11-21 16:52:45 +0100

    feat(experiment): start working on parser

---
+++ experiments/bachelor-decoupled-wmis/parser.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+import re
+
+from lab.parser import Parser
+
+class DecouplingParser(Parser):
+    def __init__(self):
+        Parser.__init__(self)
+        self.add_pattern('number_leaf_factors', 'Number leaf factors: (.+)', required=False, type=int)
