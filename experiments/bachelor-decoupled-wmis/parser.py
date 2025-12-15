e71f71e91e12b8d0a0a8bb8bae35232541871832
LazyStability <LazilyStable@proton.me>
2025-12-15 11:49:49 +0100

    wip

--- experiments/bachelor-decoupled-wmis/parser.py
+++ experiments/bachelor-decoupled-wmis/parser.py
@@ -4,8 +4,15 @@ import re
 
 from lab.parser import Parser
 
+def number_of_leaves(content, props):
+    props[""]
+    if re.search("Number final leaf candidates: (.+)")
+def chils_edges(content, props):
+    matches = re.findall(r"chils add (?:outside precondition|leaf intersection) edge between: (.+)", content)
+    props["All edges"] = [(int(s) for s in matches)]
 class MwisParser(Parser):
     def __init__(self):
         Parser.__init__(self)
         self.add_pattern('number_leaf_factors', 'Number leaf factors: (.+)', required=False, type=int)
-        self.add_pattern('mwis_weight', 'Weight of computed independent set: (.+)',required=False, type=int)
+        # self.add_pattern('mwis_weight', 'Weight of computed independent set: (.+)',required=False, type=int)
+        # self.add_pattern('mwis_size', 'Size of computed independent set: (.+)',required=False, type=int)
